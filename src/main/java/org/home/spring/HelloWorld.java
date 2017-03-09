077acf44af6a27257807cdb2d981cf35e89f3c1d
Akhila Mupparaju <Akhila Mupparaju>
2017-03-08 23:16:59 -0500

    first commit, hello world

---
+++ src/main/java/org/home/spring/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.home.spring;
+
+/**
+ * Created by Akhila on 3/8/2017.
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("Hello Wolrd");
+    }
+
+}
